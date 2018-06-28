5c29612dc77ee206b3792014d28780c94c03504a
Ahmed Almansouri <am.almansouri@gmail.com>
2018-06-28 07:18:52 +0400

    First commit

---
+++ FileSize/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  FileSize
+//
+//  Created by Ahmed Almansouri on 6/25/18.
+//  Copyright © 2018 Mopa. All rights reserved.
+//
+
+import Cocoa
+
+
+class ViewController: NSViewController {
+    
+    @IBOutlet var ScanAll: NSButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        // Do any additional setup after loading the view.
+       
+        
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
